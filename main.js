8223ac599937159e96397789cb92b6ff6bc65430
AdeWYyse <adelinewyse@gmail.com>
2023-08-02 13:27:04 -0300

    PathFix

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 
-import * as THREE from 'three';
+import * as THREE from 'three/build/three.module.js';
 
-import Stats from 'three/addons/libs/stats.module.js';
+import Stats from 'three/examples/jsm/libs/stats.module.js';
 
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { FBXLoader } from 'three/addons/loaders/FBXLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js';
 
 let camera, scene, renderer, stats;
 
